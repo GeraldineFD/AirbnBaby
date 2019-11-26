30d4ba5fd2ea3bad740eebe47e201dcabbb1f03c
Corentin Cuyvers <corentincuyvers123@gmail.com>
2019-11-26 17:02:40 +0100

    material controller is done, added 1 view(edit), fixed bugs

--- app/models/baby_material.rb
+++ app/models/baby_material.rb
@@ -1,8 +1,8 @@
 class BabyMaterial < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :sales
   validates :title, presence: true
   validates :description, presence: true
   validates :price, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }, allow_nil: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :baby_materials
-  has_many :sales
+  has_many :baby_materials, dependent: :destroy
+  has_many :sales, dependent: :destroy
 end
