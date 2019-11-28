268c9e10fc2a06de460ea22397b98b9920fec075
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2019-11-28 18:16:15 +0100

    added transaction method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'baby_materials#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :baby_materials
+  resources :baby_materials do
+    resources :sales, only: [:create]
+  end
+
 
   namespace :admin do
     get 'baby_material/index', to: "baby_materials#index"
